603a62bfe461843c3b75b8d95faaa4028f0d6ba1
flipped0513 <43701372+flipped0513@users.noreply.github.com>
2019-09-17 21:46:57 +0800

    Update account.js

--- BackStage/routes/account.js
+++ BackStage/routes/account.js
@@ -46,7 +46,7 @@ router.post('/login', urlencodedParser, function (req, res, next) {
 });
 
 /*
- * @function 新用户注册
+ * @function 新用户注册/修改
  * @param nickname(string)昵称，phone(string)电话,uid(string)学号,
  * major(string)院系，address(string)住址,password(string)密码
  * @return code(int) , msg(string)
@@ -63,11 +63,16 @@ router.post('/register', urlencodedParser, function (req, res, next) {
 	
 	let enrollmentCollection = informationDB.getCollection("user");
 	enrollmentCollection.findOne({uid: submitData.uid}, function (err, data) {
-		if (!data) {  
+		if(data){
+			enrollmentCollection.save(subimitData)
+            res.status(200).json({"code":1,"msg":"修改成功"});
+		}
+
+		else if (!data) {  
 			enrollmentCollection.insert(submitData);  
 			res.status(200).json({ "code": 1 ,"msg": "提交成功"});
 		}
-		else{   
+		    else{   
 			res.status(200).json({
 				code : -1,
 				msg : "fail"
@@ -76,6 +81,42 @@ router.post('/register', urlencodedParser, function (req, res, next) {
 
 	});
 });
+
+
+/*
+ * @function 管理员登录
+ * @param account(string) 账户, password(string) 密码
+ * @return code(int) , msg(string)
+ */
+router.post('/admin/login', urlencodedParser, function (req, res, next) {
+	let UserData = {
+		uid: req.body.uid,
+		password: req.body.password
+	}
+	
+	let accountCollection = informationDB.getCollection("user");
+	accountCollection.findOne({uid: UserData.uid}, function (err, data) {
+		if (data) {
+			if (UserData.password == data.password){
+				res.status(200).json({ "code": 1 ,"msg": "登陆成功"})
+			}
+			else {
+				res.status(200).json({ "code": -1 ,"msg": "密码错误"})
+			}
+		}
+		else{
+            res.status(200).json({"code":-1,"msg":"你不是管理员"})
+
+        }
+
+	});
+});
+
+
+
+
+
+
 
 
 
