50a7df4b77e4deac6226f4981029b3689474d60e
flipped0513 <43701372+flipped0513@users.noreply.github.com>
2019-09-23 09:19:39 +0800

    Update forum.js

--- BackStage/routes/forum.js
+++ BackStage/routes/forum.js
@@ -18,7 +18,7 @@ router.all('*', function(req, res, next) {
 /*
 发帖
 */
-router.post('/post',urlencodedParser,function(req,res,next){
+router.post('forum/post',urlencodedParser,function(req,res,next){
 	var userCollection = informationDB.getCollection("user");
 	var postCollection = informationDB.getCollection("post");
 	var now = new Date();
@@ -74,7 +74,7 @@ router.post('/post',urlencodedParser,function(req,res,next){
 /*
 评论帖子
 */
-router.post("/discuss",urlencodedParser,function(req,res,next){
+router.post("forum/discuss",urlencodedParser,function(req,res,next){
 	var postCollection = informationDB.getCollection("post");
 	var commentCollection = informationDB.getCollection("comment");
 	var discussBody = {
@@ -118,7 +118,7 @@ router.post("/discuss",urlencodedParser,function(req,res,next){
 /*
 点赞评论
 */
-router.post('/like',urlencodedParser,function(req,res,next){
+router.post('forum/like',urlencodedParser,function(req,res,next){
 	var commentCollection = informationDB.getCollection('comment');
 	var userCollection = informationDB.getCollection('user');
 
@@ -183,7 +183,7 @@ router.post('/like',urlencodedParser,function(req,res,next){
 /*
 查看所有帖子
 */
-router.get('/checkAllPost',urlencodedParser,function(req,res,next){
+router.get('forum/checkAllPost',urlencodedParser,function(req,res,next){
 	var postCollection = informationDB.getCollection('post');
 	postCollection.find().toArray(function(err,allData){
 		if(err)
@@ -202,11 +202,56 @@ router.get('/checkAllPost',urlencodedParser,function(req,res,next){
 	})
 });
 
+/*
+打开单个帖子
+*/
+router.get('forum/openOnePost',urlencodedParser,function(req,res,next){
+	var postCollection = informationDB.getCollection('post');
+	postCollection.find({_id:objectID(req.body._id)}).toArray(function(err,allData){
+		if(err)
+		{
+			res.status(200).json({
+				code: -1,
+				msg	: "openOnePost error"
+			});
+			return;
+		}
+		res.status(200).json({
+			code: 1,
+			data: allData
+		});
+		return;
+	})
+});
 
 
+/*
+查看个人所有帖子
+*/
+router.get('forum/checkPersonalPost',urlencodedParser,function(req,res,next){
+	var postCollection = informationDB.getCollection('post');
+	postCollection.find({_id:req.body._id}).toArray(function(err,allData){
+		if(err)
+		{
+			res.status(200).json({
+				code: -1,
+				msg	: "checkPersonalPost error"
+			});
+			return;
+		}
+		res.status(200).json({
+			code: 1,
+			data: allData
+		});
+		return;
+	})
+});
 
+/*
+取消评论点赞
+*/
 
-router.post('/cancelLike',urlencodedParser,function(req,res,next){
+router.post('forum/cancelLike',urlencodedParser,function(req,res,next){
 	var commentCollection = informationDB.getCollection('comment');
 	var userCollection = informationDB.getCollection('user');
 
@@ -225,7 +270,7 @@ router.post('/cancelLike',urlencodedParser,function(req,res,next){
 				return;
 			}
 		console.log("find like for comment success!");
-		commentCollection.like.deleteOne({_id:likeBody.liker},function(err,deleteResCom){
+		findResCom.like.deleteOne({_id:likeBody.liker},function(err,deleteResCom){
 			if(err)
 			{
 				res.status(200).json({
@@ -257,6 +302,242 @@ router.post('/cancelLike',urlencodedParser,function(req,res,next){
 			})
 		})
 	})
+});
+
+
+
+//意见反馈
+
+router.post('/Opinion/send', urlencodedParser, function (req, res, next) {
+
+	let postmessage = {
+
+        content: req.body.content,
+
+        reply: [],
+
+		tags: req.body.tags,
+
+		color: req.body.color,
+
+		size: req.body.size,
+
+		index: req.body.index
+
+	}
+
+
+
+	console.log(req.body)
+
+	postmessage.tags = JSON.parse(postmessage.tags)
+
+	postmessage.tags = Array.from(new Set(postmessage.tags));
+
+
+
+	console.log(postmessage)
+
+	
+
+    let opinionCollection = informationDB.getCollection("opinion");
+
+    let opinionTagCollection = informationDB.getCollection("OpinionTag");
+
+
+
+    opinionCollection.insert(postmessage);
+
+
+
+	opinionCollection.findOne({}, function (err, data) {
+
+		if (data) {
+
+            for(x in postmessage.tags){
+
+				(function(x){
+
+					console.log(postmessage.tags[x])
+
+					opinionTagCollection.findOne({tag: postmessage.tags[x]}, function (err, tagData) {
+
+						if(tagData) {
+
+							console.log(tagData)
+
+							opinionCollection.findOne({index: postmessage.index}, function (err, data) {
+
+								tagData.messages.push(data);
+
+								opinionTagCollection.update({tag: postmessage.tags[x]}, {$set:{'messages': tagData.messages}})
+
+							});
+
+						}
+
+						else{
+
+							let tagItem = {
+
+								tag: postmessage.tags[x],
+
+								messages: []
+
+							}
+
+							opinionCollection.findOne({index: postmessage.index}, function (err, data) {
+
+								tagItem.messages.push(data);
+
+								opinionTagCollection.insert(tagItem);
+
+							});
+
+						}
+
+					});
+
+				}) (x);
+
+            }
+
+            res.status(200).json({ "code": "1" ,"msg": "提交成功"})
+
+		}
+
+		else{
+
+			res.status(200).json({ "code": "-1" ,"msg": "提交失败"})
+
+		}
+
+		
+
+    });
+
+
+
+});
+
+
+
+//意见反馈
+
+router.post('/Opinion/Reply', urlencodedParser, function (req, res, next) {
+
+	let replymessage = {
+
+        content: req.body.content,
+
+		index: req.body.index,
+
+		name: req.body.name
+
+	}
+
+
+
+	console.log(req.body)
+
+	
+
+    let opinionCollection = informationDB.getCollection("opinion");
+
+
+
+	opinionCollection.findOne({index: replymessage.index}, function (err, data) {
+
+		if (data) {
+
+			data.reply.push(replymessage)
+
+            opinionCollection.update({index: replymessage.index}, {$set:{'reply': data.reply}})
+
+            res.status(200).json({ "code": "1" ,"msg": "提交成功"})
+
+        }
+
+        else{
+
+            res.status(200).json({ "code": "-1" ,"msg": "没有这条消息"})
+
+        }
+
+    });
+
+
+
+});
+
+
+
+//意见反馈
+
+router.get('/Opinion', urlencodedParser, function (req, res, next) {
+
+
+
+    let opinionCollection = informationDB.getCollection("opinion");
+
+
+
+	opinionCollection.find().sort({"_id":-1}).toArray(function (err, allData) {
+
+        res.status(200).json({data: allData})
+
+	})
+
+
+
+});
+
+
+
+//意见反馈
+
+router.get('/Opinion/getTags', urlencodedParser, function (req, res, next) {
+
+
+
+    let opinionTagCollection = informationDB.getCollection("opinion");
+
+
+
+	opinionTagCollection.find().toArray(function (err, allData) {
+
+        res.status(200).json({data: allData})
+
+	})
+
+
+
+});
+
+
+
+//意见反馈
+
+router.get('/Opinion/getByTag', urlencodedParser, function (req, res, next) {
+
+
+
+	let tag = req.query.tag;
+
+
+
+	let opinionTagCollection = informationDB.getCollection("opinion");
+
+
+
+	opinionTagCollection.find({tag: tag}).sort({"_id":-1}).toArray(function (err, allData) {
+
+        res.status(200).json({data: allData})
+
+	})
+
+
+
 });
 
 
